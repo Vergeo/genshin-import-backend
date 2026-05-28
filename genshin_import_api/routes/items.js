7951ca7493e33b87ffa2037b2846cfcfdd54ebf7
Vergeo <vergeovalenting@gmail.com>
2026-05-28 16:38:34 +0700

    update endpoint path

--- genshin_import_api/routes/items.js
+++ genshin_import_api/routes/items.js
@@ -1,112 +1,121 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const {initializeDatabase, getPool} = require('../database/db');
+const { initializeDatabase, getPool } = require("../database/db");
 
-router.get('/get-all-items', async (req, res) => {
-  try {
-    const [result] = await getPool().query(
-      `SELECT * 
-      FROM items`
-    )
-    res.status(200).json(result);
-  } catch (err) {
-    res.status(500).json({'message': 'failed to fetch items'});
-  }
-})
+router.get("/get-all-items", async (req, res) => {
+	try {
+		const [result] = await getPool().query(
+			`SELECT * 
+      FROM items`,
+		);
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(500).json({ message: "failed to fetch items" });
+	}
+});
 
-router.get('/get-items/:item_id', async (req, res) => {
-  try {
-    const {item_id} = req.params;
-    const [result] = await getPool().query(
-      `SELECT * 
+router.get("/get-item/:item_id", async (req, res) => {
+	try {
+		const { item_id } = req.params;
+		const [result] = await getPool().query(
+			`SELECT * 
       FROM items 
       WHERE item_id = ?`,
-      [item_id]
-    )
-    if(result.length === 0){
-      return res.status(404).json({'message': 'item not found'});
-    }
-    res.status(200).json(result);
-  } catch (err) {
-    res.status(500).json({'message': 'failed to fetch item'});
-  }
-})
+			[item_id],
+		);
+		if (result.length === 0) {
+			return res.status(404).json({ message: "item not found" });
+		}
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(500).json({ message: "failed to fetch item" });
+	}
+});
 
-router.get('/get-items-by-type/:item_type', async (req, res) => {
-  try {
-    const {item_type} = req.params;
-    const [result] = await getPool().query(
-      `SELECT * 
+router.get("/get-items-by-type/:item_type", async (req, res) => {
+	try {
+		const { item_type } = req.params;
+		const [result] = await getPool().query(
+			`SELECT * 
       FROM items 
       WHERE item_type = ?`,
-      [item_type]
-    )
-    if(result.length === 0){
-      res.status(404).json({'message': 'item not found'});
-    }
-    res.status(200).json(result);
-  } catch(err){
-    res.status(500).json({'message': 'failed to fetch item'});
-  }
-})
+			[item_type],
+		);
+		if (result.length === 0) {
+			res.status(404).json({ message: "item not found" });
+		}
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(500).json({ message: "failed to fetch item" });
+	}
+});
 
-router.get('/get-empty-stock', async (req, res) => {
-  try {
-    const [result] = await getPool().query(
-      `SELECT * 
+router.get("/get-empty-stock", async (req, res) => {
+	try {
+		const [result] = await getPool().query(
+			`SELECT * 
       FROM items 
-      WHERE item_stock <= 0`
-    )
-    res.status(200).json(result);
-  } catch(err){
-    res.status(500).json({'message': 'failed to fetch item'});
-  }
-})
+      WHERE item_stock <= 0`,
+		);
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(500).json({ message: "failed to fetch item" });
+	}
+});
 
-router.get('/search/:item_name', async (req, res) => {
-  try {
-    const {item_name} = req.params;
-    const [result] = await getPool().query(
-      `SELECT * 
+router.get("/search/:item_name", async (req, res) => {
+	try {
+		const { item_name } = req.params;
+		const [result] = await getPool().query(
+			`SELECT * 
       FROM items 
       WHERE item_name = ?`,
-      [item_name]
-    )
-    if(result.length === 0){
-      res.status(404).json({'message': 'item not found'});
-    }
-    res.status(200).json(result);
-  } catch(err){
-    res.status(500).json({'message': 'failed to fetch item'});
-  }
-})
+			[item_name],
+		);
+		if (result.length === 0) {
+			res.status(404).json({ message: "item not found" });
+		}
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(500).json({ message: "failed to fetch item" });
+	}
+});
 
-router.post('/create-items', async (req, res) => {
-  try {
-    const {item_name, item_type, item_description, item_stock, item_image, item_price} = req.body;
-    const [result] = await getPool().query(
-      `INSERT INTO items (item_name, item_type, item_description, item_stock, item_image, item_price) 
+router.post("/create-item", async (req, res) => {
+	try {
+		const { item_name, item_type, item_description, item_stock, item_image, item_price } =
+			req.body;
+		const [result] = await getPool().query(
+			`INSERT INTO items (item_name, item_type, item_description, item_stock, item_image, item_price) 
       VALUES (?, ?, ?, ?, ?, ?)`,
-      [item_name, item_type, item_description, item_stock, item_image, item_price]
-    )
-    const item_id = result.insertId;
-    res.status(201).json({item_id, item_name, item_type, item_description, item_stock, item_image, item_price});
-  } catch(err) {
-    res.status(500).json({'message': 'failed to create item'});
-  }
-})
+			[item_name, item_type, item_description, item_stock, item_image, item_price],
+		);
+		const item_id = result.insertId;
+		res.status(201).json({
+			item_id,
+			item_name,
+			item_type,
+			item_description,
+			item_stock,
+			item_image,
+			item_price,
+		});
+	} catch (err) {
+		res.status(500).json({ message: "failed to create item" });
+	}
+});
 
-router.patch('/update-items/:item_id', async (req, res) => {
-  try {
-    const {item_id} = req.params;
-    const item_name = req.body.item_name ?? null;
-    const item_type = req.body.item_type ?? null;
-    const item_description = req.body.item_description ?? null;
-    const item_stock = req.body.item_stock ?? null;
-    const item_image = req.body.item_image ?? null;
-    const item_price = req.body.item_price ?? null;
-    const [result] = await getPool().query(
-      `UPDATE items
+router.patch("/update-item/:item_id", async (req, res) => {
+	try {
+		const { item_id } = req.params;
+		const item_name = req.body.item_name ?? null;
+		const item_type = req.body.item_type ?? null;
+		const item_description = req.body.item_description ?? null;
+		const item_stock = req.body.item_stock ?? null;
+		const item_image = req.body.item_image ?? null;
+		const item_price = req.body.item_price ?? null;
+		const [result] = await getPool().query(
+			`UPDATE items
       SET item_name = IFNULL(?, item_name),
       item_type = IFNULL(?, item_type),
       item_description = IFNULL(?, item_description),
@@ -114,32 +123,32 @@ router.patch('/update-items/:item_id', async (req, res) => {
       item_image = IFNULL(?, item_image),
       item_price = IFNULL(?, item_price)
       WHERE item_id = ?`,
-      [item_name, item_type, item_description, item_stock, item_image, item_price, item_id]
-    )
-    if(result.affectedRows === 0){
-      return res.status(404).json({'message': 'item not found'});
-    }
-    res.status(200).json({'message': 'item updated'});
-  } catch(err) {
-    res.status(500).json({'message': 'failed to update item'});
-  }
-})
+			[item_name, item_type, item_description, item_stock, item_image, item_price, item_id],
+		);
+		if (result.affectedRows === 0) {
+			return res.status(404).json({ message: "item not found" });
+		}
+		res.status(200).json({ message: "item updated" });
+	} catch (err) {
+		res.status(500).json({ message: "failed to update item" });
+	}
+});
 
-router.delete('/delete-items/:item_id', async (req, res) => {
-  try {
-    const {item_id} = req.params;
-    const [result] = await getPool().query(
-      `DELETE FROM items 
+router.delete("/delete-item/:item_id", async (req, res) => {
+	try {
+		const { item_id } = req.params;
+		const [result] = await getPool().query(
+			`DELETE FROM items 
       WHERE item_id = ?`,
-      [item_id]
-    )
-    if(result.affectedRows === 0){
-      return res.status(404).json({'message': 'item not found'});
-    }
-    res.status(200).json({'message': 'item deleted'});
-  } catch(err) {
-    res.status(500).json({'message': 'failed to delete item'});
-  }
-})
+			[item_id],
+		);
+		if (result.affectedRows === 0) {
+			return res.status(404).json({ message: "item not found" });
+		}
+		res.status(200).json({ message: "item deleted" });
+	} catch (err) {
+		res.status(500).json({ message: "failed to delete item" });
+	}
+});
 
 module.exports = router;
--- genshin_import_api/routes/sales.js
+++ genshin_import_api/routes/sales.js
@@ -1,116 +1,116 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const {initializeDatabase, getPool} = require('../database/db');
+const { initializeDatabase, getPool } = require("../database/db");
 
-router.get('/get-all-sales', async (req, res) => {
-    try {
-        const [result] = await getPool().query(
-            `SELECT * 
-            FROM sales`
-        )
-        res.status(200).json(result);
-    } catch(err){
-        res.status(500).json({'message': 'failed to fetch sale'});
-    }
-})
+router.get("/get-all-sales", async (req, res) => {
+	try {
+		const [result] = await getPool().query(
+			`SELECT * 
+            FROM sales`,
+		);
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(500).json({ message: "failed to fetch sale" });
+	}
+});
 
-router.get('/get-sales/:sales_id', async (req, res) => {
-    try {
-        const {sales_id} = req.params;
-        const [result] = await getPool().query(
-            `SELECT * 
+router.get("/get-sale/:sales_id", async (req, res) => {
+	try {
+		const { sales_id } = req.params;
+		const [result] = await getPool().query(
+			`SELECT * 
             FROM sales 
             WHERE sales_id = ?`,
-            [sales_id]
-        )
-        if(result.length === 0){
-            return res.status(404).json({'message': 'sale not found'});
-        }
-        res.status(200).json(result);
-    } catch(err){
-        res.status(500).json({'message': 'failed to fetch sale'});
-    }
-})
+			[sales_id],
+		);
+		if (result.length === 0) {
+			return res.status(404).json({ message: "sale not found" });
+		}
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(500).json({ message: "failed to fetch sale" });
+	}
+});
 
-router.get('/get-sales-by-users/:user_id', async (req, res) => {
-    try {
-        const {user_id} = req.params;
-        const [result] = await getPool().query(
-            `SELECT * 
+router.get("/get-sales-by-users/:user_id", async (req, res) => {
+	try {
+		const { user_id } = req.params;
+		const [result] = await getPool().query(
+			`SELECT * 
             FROM sales 
             WHERE user_id = ?`,
-            [user_id]
-        )
-        if(result.length === 0){
-            res.status(404).json({'message': 'sale not found'});
-        }
-        res.status(200).json(result);
-    } catch(err){
-        res.status(500).json({'message': 'failed to fetch sale'});
-    }
-})
+			[user_id],
+		);
+		if (result.length === 0) {
+			res.status(404).json({ message: "sale not found" });
+		}
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(500).json({ message: "failed to fetch sale" });
+	}
+});
 
-router.get('/get-top-sales', async (req, res) => {
-    try {
-        const [result] = await getPool().query(
-            `SELECT *
+router.get("/get-top-sales", async (req, res) => {
+	try {
+		const [result] = await getPool().query(
+			`SELECT *
             FROM sales
             ORDER BY quantity DESC
-            LIMIT 10`
-        )
-        res.status(200).json(result);
-    } catch(err){
-        res.status(500).json({'message': 'failed to fetch sale'});
-    }
-})
+            LIMIT 10`,
+		);
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(500).json({ message: "failed to fetch sale" });
+	}
+});
 
-router.post('/create-sales', async (req, res) => {
-    try {
-        const {user_id, item_id, quantity} = req.body;
-        const [result] = await getPool().query(
-            `INSERT INTO sales (user_id, item_id, quantity)
+router.post("/create-sale", async (req, res) => {
+	try {
+		const { user_id, item_id, quantity } = req.body;
+		const [result] = await getPool().query(
+			`INSERT INTO sales (user_id, item_id, quantity)
             VALUES (?, ?, ?)`,
-            [user_id, item_id, quantity]
-        )
-        const sales_id = result.insertId;
-        res.status(201).json({sales_id, user_id, item_id, quantity});
-    } catch(err) {
-        res.status(500).json({'message': 'failed to create sales'});
-    }
-})
+			[user_id, item_id, quantity],
+		);
+		const sales_id = result.insertId;
+		res.status(201).json({ sales_id, user_id, item_id, quantity });
+	} catch (err) {
+		res.status(500).json({ message: "failed to create sales" });
+	}
+});
 
-router.patch('/patch/:sales_id', async (req, res) => {
-    try {
-        const {sales_id} = req.params;
-        const {user_id, item_id, quantity} = req.body;
-        const [result] = await getPool().query(
-            'UPDATE sales SET user_id = ?, item_id = ?, quantity = ? WHERE sales_id = ?',
-            [user_id, item_id, quantity, sales_id]
-        )
-        if(result.affectedRows === 0){
-            return res.status(404).json({'message': 'sale not found'});
-        }
-        res.status(200).json({'message': 'sale updated'});
-    } catch(err){
-        res.status(500).json({'message': 'failed to update sale'});
-    }
-})
+router.patch("/update-sale/:sales_id", async (req, res) => {
+	try {
+		const { sales_id } = req.params;
+		const { user_id, item_id, quantity } = req.body;
+		const [result] = await getPool().query(
+			"UPDATE sales SET user_id = ?, item_id = ?, quantity = ? WHERE sales_id = ?",
+			[user_id, item_id, quantity, sales_id],
+		);
+		if (result.affectedRows === 0) {
+			return res.status(404).json({ message: "sale not found" });
+		}
+		res.status(200).json({ message: "sale updated" });
+	} catch (err) {
+		res.status(500).json({ message: "failed to update sale" });
+	}
+});
 
-router.delete('/delete-sales/:sales_id', async (req, res) => {
-    try {
-        const {sales_id} = req.params;
-        const [result] = await getPool().query(
-            `DELETE FROM sales
+router.delete("/delete-sale/:sales_id", async (req, res) => {
+	try {
+		const { sales_id } = req.params;
+		const [result] = await getPool().query(
+			`DELETE FROM sales
             WHERE sales_id = ?`,
-            [sales_id]
-        )
-        if(result.affectedRows === 0){
-            return res.status(404).json({'message': 'sale not found'});
-        }
-        res.status(200).json({'message': 'sale deleted'});
-    } catch(err) {
-        res.status(500).json({'message': 'failed to delete sale'});
-    }
-})
+			[sales_id],
+		);
+		if (result.affectedRows === 0) {
+			return res.status(404).json({ message: "sale not found" });
+		}
+		res.status(200).json({ message: "sale deleted" });
+	} catch (err) {
+		res.status(500).json({ message: "failed to delete sale" });
+	}
+});
 
 module.exports = router;
